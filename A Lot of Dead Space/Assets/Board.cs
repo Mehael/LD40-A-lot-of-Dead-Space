605cdbb8b94a07ce71484985966a9f0190fd89a7
Mehael <mehael@mail.ru>
2017-12-02 19:10:17 +1000

    Add a grid with an draggale object.

---
+++ A Lot of Dead Space/Assets/Board.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Board : MonoBehaviour {
+    public static Board instance;
+
+    public int Width = 10;
+    public int Height = 10;
+    public Transform LinePrefub;
+
+    int _width;
+    int _height;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        RedrawBoard();
+    }
+
+    void Update () {
+        if (_width != Width || _height != Height)
+            RedrawBoard();
+	}
+
+    private void RedrawBoard()
+    {
+        var lines = new List<Transform>();
+        for (int i = 0; i < transform.childCount; i++)
+            lines.Add(transform.GetChild(i));
+
+        foreach (var line in lines)
+            DestroyImmediate(line.gameObject);
+
+        //Draw new
+        var angle = Quaternion.Euler(0, 0, 90);
+        for (int x = 0; x <= Width; x++)
+            Instantiate(LinePrefub, new Vector3(x,0,0) + transform.position, angle, transform);
+
+        for (int y = 0; y <= Height; y++)
+            Instantiate(LinePrefub, new Vector3(0, y, 0) + transform.position, Quaternion.identity, transform);
+
+        _width = Width;
+        _height = Height;
+    }
+
+    internal Vector3 GetClosestSnap(Vector3 vector3)
+    {
+        var localCoords = vector3 - transform.localPosition;
+        var localShift = transform.position - 
+            new Vector3(Mathf.RoundToInt(transform.position.x) + 0.5f,
+            Mathf.RoundToInt(transform.position.y) + 0.5f);
+
+        var snappedCoords = new Vector3(
+            Mathf.RoundToInt(localCoords.x) + transform.localPosition.x + localShift.x,
+            Mathf.RoundToInt(localCoords.y) + transform.localPosition.y + localShift.y,
+            0);
+
+        return snappedCoords;
+    }
+}
---
+++ A Lot of Dead Space/Assets/MovingObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MovingObject : EventTrigger {
+    Vector3 startPosition;
+
+    public override void OnBeginDrag(PointerEventData data)
+    {
+        startPosition = transform.position;
+    }
+
+    public override void OnDrag(PointerEventData data)
+    {
+        transform.position = GetMouseCoords();
+    }
+
+    private static Vector3 GetMouseCoords()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        return new Vector3(ray.origin.x, ray.origin.y, 0);
+    }
+
+    public override void OnEndDrag(PointerEventData data)
+    {
+        transform.position = Board.instance.GetClosestSnap(GetMouseCoords());
+    }
+}
