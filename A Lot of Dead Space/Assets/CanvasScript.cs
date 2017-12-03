5dd7173a5cc457bc37acb0418db541fb49f9067f
Mehael <mehael@mail.ru>
2017-12-03 11:33:06 +1000

    Add autohidding for ingame gizmos.

--- A Lot of Dead Space/Assets/CanvasScript.cs
+++ A Lot of Dead Space/Assets/CanvasScript.cs
@@ -1,7 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public static class CanvasScript{
+public class CanvasScript : MonoBehaviour {
     public static float scaleFactor = 37f;
+
+    public static List<InGameGizmo> inGameGizmos =
+        new List<InGameGizmo>();
+
+    public float timeBeforeHidingGUI = 2f;
+    public float hidingSpeed = 0.8f;
+    public float showingSpeed = 2f;
+    public float mouseEpsilon = 1.5f;
+
+    float timer = 0;
+    Vector3 lastMousePosition = Vector3.zero;
+    bool isHidden = false;
+
+    private void Update()
+    {
+        var newMousePosition = Input.mousePosition;
+        if ((lastMousePosition - newMousePosition).magnitude > mouseEpsilon ||
+            Input.GetMouseButtonDown(0))
+        {
+            timer = 0;
+            isHidden = false;
+            foreach (var gizmo in inGameGizmos)
+                gizmo.Show(showingSpeed);
+        }
+        else if (!isHidden)
+        {
+            if (timer >= timeBeforeHidingGUI)
+            {
+                isHidden = true;
+                foreach (var gizmo in inGameGizmos)
+                    gizmo.Hide(hidingSpeed);
+            }
+            else if (!Input.GetMouseButton(0))
+                timer += Time.deltaTime;
+        }
+
+        lastMousePosition = newMousePosition;
+    }
 }
---
+++ A Lot of Dead Space/Assets/InGameGizmo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InGameGizmo : MonoBehaviour {
+    Image image;
+    float maxAlpha;
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+        maxAlpha = image.color.a;
+        CanvasScript.inGameGizmos.Add(this);
+    }
+
+    private void OnDestroy()
+    {
+        image = GetComponent<Image>();
+        CanvasScript.inGameGizmos.Remove(this);
+    }
+
+    float alphaVelocity = 0f;
+    private void Update()
+    {
+        if (alphaVelocity == 0f) return;
+
+        image.color = new Color(image.color.r, 
+            image.color.g,
+            image.color.b,
+            Mathf.Clamp(image.color.a+alphaVelocity*Time.deltaTime, 0, maxAlpha));
+
+        if (image.color.a == 0 && alphaVelocity < 0)
+            alphaVelocity = 0;
+        else if (image.color.a == maxAlpha && alphaVelocity > 0)
+            alphaVelocity = 0;
+    }
+
+    internal void Show(float speed)
+    {
+        alphaVelocity = speed;
+    }
+
+    internal void Hide(float speed)
+    {
+        alphaVelocity = -speed;
+    }
+}
