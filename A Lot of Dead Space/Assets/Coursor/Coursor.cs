ef7fa786509040a64ad6cc71b735f779458239a7
Mehael <mehael@mail.ru>
2017-12-05 00:31:06 +1000

    Add coursors animator.

---
+++ A Lot of Dead Space/Assets/Coursor/Coursor.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coursor : MonoBehaviour {
+    public void Kill(string param) { Destroy(gameObject);  }
+}
