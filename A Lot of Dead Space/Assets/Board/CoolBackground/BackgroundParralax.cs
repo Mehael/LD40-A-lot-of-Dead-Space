b6b2ed47f513ebe90eae647452b9cd0c3e89ffe4
Mehael <mehael@mail.ru>
2017-12-05 01:20:32 +1000

    Implement backgrounds.

---
+++ A Lot of Dead Space/Assets/Board/CoolBackground/BackgroundParralax.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundParralax : MonoBehaviour {
+    public Vector3 shift;
+
+    Vector3 startPos;
+    private void Awake()
+    {
+        startPos = transform.position;
+    }
+
+    void LateUpdate () {
+        transform.position = startPos +
+            (Input.mousePosition.x / Screen.width - 0.5f) * shift; 		
+	}
+}
